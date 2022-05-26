28156f9e5f8205be88c67ba69be00999009f6099
Ignacio Agramont <ignacio.agramont.11@gmail.com>
2022-05-26 13:40:57 -0400

    Agregar todas las rutas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,7 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { PruebaComponent } from './prueba/prueba.component';
-import { NuevaVentaComponent } from './nueva-venta/nueva-venta.component'
+import { NuevaVentaComponent } from './nueva-venta/nueva-venta.component';
+import { DetalleVentaComponent } from './detalle-venta/detalle-venta.component';
+import { AgregarProductoComponent } from './agregar-producto/agregar-producto.component';
+import { EliminarProductoComponent } from './eliminar-producto/eliminar-producto.component';
+import { ModificarProductoComponent } from './modificar-producto/modificar-producto.component';
+import { ContactosComponent } from './contactos/contactos.component';
+import { AgregarContactoComponent } from './agregar-contacto/agregar-contacto.component';
+import { EliminarContactoComponent } from './eliminar-contacto/eliminar-contacto.component';
+import { ModificarContactoComponent } from './modificar-contacto/modificar-contacto.component';
+
 
 const routes: Routes = [
   {path: 'ventas', 
@@ -10,6 +19,30 @@ const routes: Routes = [
 
   {path: 'ventas/nueva',
   component: NuevaVentaComponent},  
+
+  {path: 'ventas/detalle',
+  component: DetalleVentaComponent},  
+
+  {path: 'productos/agregar',
+  component: AgregarProductoComponent},  
+  
+  {path: 'productos/eliminar',
+  component: EliminarProductoComponent},  
+
+  {path: 'productos/modificar',
+  component: ModificarProductoComponent},
+
+  {path: 'contactos/todos',
+  component: ContactosComponent},
+
+  {path: 'contactos/agregar',
+  component: AgregarContactoComponent},
+
+  {path: 'contactos/eliminar',
+  component: EliminarContactoComponent},
+
+  {path: 'contactos/modificar',
+  component: ModificarContactoComponent},
 
   {path: 'footer', 
   component: FooterComponent},
