de0be4d94a69b15cb4ce2ab304c8879e33cf67c2
Ignacio Agramont <ignacio.agramont.11@gmail.com>
2022-05-26 13:23:37 -0400

    routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { PruebaComponent } from './prueba/prueba.component';
+import { NuevaVentaComponent } from './nueva-venta/nueva-venta.component'
 
 const routes: Routes = [
   {path: 'ventas', 
-  component: PruebaComponent },
+  component: PruebaComponent},
+
+  {path: 'ventas/nueva',
+  component: NuevaVentaComponent},  
 
   {path: 'footer', 
   component: FooterComponent},
