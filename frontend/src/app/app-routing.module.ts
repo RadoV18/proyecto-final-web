d63003535ea392769aa66907a0933ef485f78f8c
Ignacio Agramont <ignacio.agramont.11@gmail.com>
2022-05-26 12:19:20 -0400

    rutas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', 
+  component: AppComponent},
+
+  {path: 'toolbar', 
+  component: ToolbarComponent},
+  
+  {path: 'ventas', 
+  component: SidebarComponent },
+  
+  {path: 'footer', 
+  component: FooterComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
