c822a5a96a23dc8961c457280887d21ea065994d
Radomir Vladislavic <vladislavic.rl@gmail.com>
2022-05-26 13:38:41 -0400

    endpoints de products y models

---
+++ backend/models/contact.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const contactsSchema = new mongoose.Schema({
+    name: String,
+    lastName: String,
+    ci: Number,
+    email: String,
+    phone: Number
+});
+
+contactsSchema.set("toJSON", (document, returnedContact) => {
+    returnedContact.id = returnedContact._id.toString();
+    delete returnedContact._id;
+    delete returnedContact.__v;
+});
+
+module.exports = mongoose.model('Contact', contactsSchema);
---
+++ backend/models/sale.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const salesSchema = new Schema({
+    timestamp: {
+        type: Date,
+        default: Date.now
+    },
+    contactId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Contact'
+    },
+    products: [
+        {
+            productId: {
+                type: Schema.Types.ObjectId,
+                ref: 'Product'
+            },
+            quantity: {
+                type: Number,
+                default: 0
+            }
+        }
+    ]
+});
+
+salesSchema.set("toJSON", (document, returnedSale) => {
+    returnedSale.id = returnedSale._id.toString();
+    delete returnedSale._id;
+    delete returnedSale.__v;
+});
+
+module.exports = mongoose.model("Sale", salesSchema);
