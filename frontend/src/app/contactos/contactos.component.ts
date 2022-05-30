49fd3e96858133790a67e5af21d2c261e72a995e
Sebastian Francisco Belmonte Cervero <sebastian.belmonte01@gmail.com>
2022-05-30 16:27:58 -0400

    Subiendo lista contacto corregida

--- frontend/src/app/contactos/contactos.component.ts
+++ frontend/src/app/contactos/contactos.component.ts
@@ -2,27 +2,25 @@ import { Component, OnInit, Input } from '@angular/core';
 
 
 export interface PeriodicElement {
-  name: string;
+  nombre: string;
   position: number;
-  weight: number;
-  symbol: string;
+  apellido: string;
+  ci: string;
   fono: number;
   email: string;
-
-
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H', fono: 1234, email: 'mail'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He', fono: 1234, email: 'mail'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li', fono: 1234, email: 'mail'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be', fono: 1234, email: 'mail'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B', fono: 1234, email: 'mail'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C', fono: 1234, email: 'mail'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N', fono: 1234, email: 'mail'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O', fono: 1234, email: 'mail'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F', fono: 1234, email: 'mail'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne', fono: 1234, email: 'mail'},
+  {position: 1, nombre: 'Hydrogen', apellido:'Apellido1', ci: 'H', fono: 1234, email: 'mail'},
+  {position: 2, nombre: 'Helium', apellido: 'Apellido1', ci: 'He', fono: 1234, email: 'mail'},
+  {position: 3, nombre: 'Lithium', apellido: 'Apellido1', ci: 'Li', fono: 1234, email: 'mail'},
+  {position: 4, nombre: 'Beryllium', apellido: 'Apellido1', ci: 'Be', fono: 1234, email: 'mail'},
+  {position: 5, nombre: 'Boron', apellido: 'Apellido1', ci: 'B', fono: 1234, email: 'mail'},
+  {position: 6, nombre: 'Carbon', apellido: 'Apellido1', ci: 'C', fono: 1234, email: 'mail'},
+  {position: 7, nombre: 'Nitrogen', apellido: 'Apellido1', ci: 'N', fono: 1234, email: 'mail'},
+  {position: 8, nombre: 'Oxygen', apellido: 'Apellido1', ci: 'O', fono: 1234, email: 'mail'},
+  {position: 9, nombre: 'Fluorine', apellido: 'Apellido1', ci: 'F', fono: 1234, email: 'mail'},
+  {position: 10, nombre: 'Neon', apellido: 'Apellido1', ci: 'Ne', fono: 1234, email: 'mail'},
 ];
 
 @Component({
@@ -32,7 +30,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 
 export class ContactosComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'fono', 'email'];
+  displayedColumns: string[] = ['position', 'nombre', 'apellido', 'ci', 'fono', 'email'];
   dataSource = ELEMENT_DATA;
 
   ngOnInit(): void {
