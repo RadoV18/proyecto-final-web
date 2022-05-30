45e9fe40098e185762762ff1c1efceaf4dc4b57a
Radomir Vladislavić <vladislavic.rl@gmail.com>
2022-05-29 20:11:30 -0400

    bearer authentication

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,9 @@ const salesController = require("./controllers/sales");
 const usersController = require("./controllers/users");
 const authController = require("./controllers/auth");
 
+// middleware
+const { tokenExtractor, userExtractor } = require("./utils/authMiddleware");
+
 // mongoose
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
@@ -25,11 +28,17 @@ app.use(cors());
 app.use(morgan('tiny'));
 app.use(express.json());
 
-// use controllers
+// public endpoints
+app.use("/api/auth", authController);
+app.use("/api/users", usersController);
+
+// authentication middleware
+app.use(tokenExtractor);
+app.use(userExtractor);
+
+// protected endpoints
 app.use("/api/products", productsController);
 app.use("/api/contacts", contactsController);
 app.use("/api/sales", salesController);
-app.use("/api/users", usersController);
-app.use("/api/auth", authController);
 
 module.exports = app;
