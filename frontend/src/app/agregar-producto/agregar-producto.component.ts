901447682649c335243d380e6df7fbdde7c5063b
Radomir Vladislavić <vladislavic.rl@gmail.com>
2022-05-30 20:58:12 -0400

    Correcciones en pagina para agregar productos

--- frontend/src/app/agregar-producto/agregar-producto.component.ts
+++ frontend/src/app/agregar-producto/agregar-producto.component.ts
@@ -7,22 +7,28 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class AgregarProductoComponent implements OnInit {
   @Input() url: string;
-  constructor() { 
+  constructor() {
     this.url = '/assets/image.jpg';
 
   }
 
-  selectFile(event: any){
-    if(event.target.files){
-        var reader = new FileReader();
-        reader.readAsDataURL(event.target.files[0]);
-        reader.onload = (event: any) =>{
-          this.url = event.target.result;
-          console.log(this.url);
-        }
-    }
-    else {
-       console.log("Nada");
+  openFileBrowser (event: any) {
+    event.preventDefault();
+
+    let element: HTMLElement = document.getElementById('fileupload') as HTMLElement;
+    element.click();
+  }
+
+  selectFile(event: any) {
+    if (event.target.files) {
+      var reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0]);
+      reader.onload = (event: any) => {
+        this.url = event.target.result;
+        console.log(this.url);
+      }
+    } else {
+      console.log("Nada");
     }
   }
 
