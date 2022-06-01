0651a3a59adf89d4b3346b065544ab357a465541
Radomir Vladislavić <vladislavic.rl@gmail.com>
2022-06-01 16:40:04 -0400

    Mostrar todos los contactos

--- frontend/src/app/contactos/contactos.component.ts
+++ frontend/src/app/contactos/contactos.component.ts
@@ -1,27 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, Input } from '@angular/core';
 
 
-export interface PeriodicElement {
-  nombre: string;
+export interface ContactElement {
+  name: string;
   position: number;
-  apellido: string;
+  lastName: string;
   ci: string;
-  fono: number;
+  phone: number;
   email: string;
 }
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, nombre: 'Hydrogen', apellido:'Apellido1', ci: 'H', fono: 1234, email: 'mail'},
-  {position: 2, nombre: 'Helium', apellido: 'Apellido1', ci: 'He', fono: 1234, email: 'mail'},
-  {position: 3, nombre: 'Lithium', apellido: 'Apellido1', ci: 'Li', fono: 1234, email: 'mail'},
-  {position: 4, nombre: 'Beryllium', apellido: 'Apellido1', ci: 'Be', fono: 1234, email: 'mail'},
-  {position: 5, nombre: 'Boron', apellido: 'Apellido1', ci: 'B', fono: 1234, email: 'mail'},
-  {position: 6, nombre: 'Carbon', apellido: 'Apellido1', ci: 'C', fono: 1234, email: 'mail'},
-  {position: 7, nombre: 'Nitrogen', apellido: 'Apellido1', ci: 'N', fono: 1234, email: 'mail'},
-  {position: 8, nombre: 'Oxygen', apellido: 'Apellido1', ci: 'O', fono: 1234, email: 'mail'},
-  {position: 9, nombre: 'Fluorine', apellido: 'Apellido1', ci: 'F', fono: 1234, email: 'mail'},
-  {position: 10, nombre: 'Neon', apellido: 'Apellido1', ci: 'Ne', fono: 1234, email: 'mail'},
-];
 
 @Component({
   selector: 'app-contactos',
@@ -30,10 +18,25 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 
 export class ContactosComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'nombre', 'apellido', 'ci', 'fono', 'email'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['position', 'name', 'lastName', 'ci', 'phone', 'email'];
+  dataSource: ContactElement[] = [];
+
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.http.get('http://localhost:3001/api/contacts').subscribe({
+      next: (data: any) => {
+        this.dataSource = data.map((contact: any, index: number) => {
+          return {
+            ...contact,
+            position: index + 1
+          }
+        })
+      },
+      error: (err: any) => {
+        console.error(err);
+      }
+    })
   }
 
 }
