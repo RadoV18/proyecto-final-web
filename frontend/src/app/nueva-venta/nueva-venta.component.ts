196cfb84de8ed60738864d848037a8beefce2a70
Ignacio Agramont <ignacio.agramont.11@gmail.com>
2022-06-02 14:20:17 -0400

    productos terminado

--- frontend/src/app/nueva-venta/nueva-venta.component.ts
+++ frontend/src/app/nueva-venta/nueva-venta.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { ProductCard } from '../product-card/product-card.model'
+import { PRODUCTS } from "../mock-products";
+
 
 @Component({
   selector: "app-nueva-venta",
@@ -6,22 +9,15 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./nueva-venta.component.css"],
 })
 export class NuevaVentaComponent implements OnInit {
-  products =
-    {
-      id: "1012",
-      code: 1,
-      name: "Extra Creamy Milk Chocolate EXCELLENCE Bar (3.5 oz)",
-      description:
-        "The ultimate milk chocolate experience-an exceptionally smooth and creamy recipe. EXCELLENCE Extra Creamy is a pure milk chocolate bar. Expertly crafted with the finest ingredients by the Master Chocolatiers at Lindt.",
-      brand: "IguiPene",
-      availability: "En Stock",
-      stock: 10,
-      price: 200,
-      categories: "Leche",
-      extension: "/assets/img/products/1.jpg",
-    };
-
+  
+  products = PRODUCTS;
+  selectedProduct?: ProductCard;
+    
   constructor() {}
 
   ngOnInit(): void {}
+
+  onSelect(product: ProductCard): void{
+    this.selectedProduct = product;
+  }
 }
