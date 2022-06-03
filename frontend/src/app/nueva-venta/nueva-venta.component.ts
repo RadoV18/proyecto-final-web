54718941ef5217a71a44970880c96e2b270627c8
Radomir Vladislavić <vladislavic.rl@gmail.com>
2022-06-02 22:33:08 -0400

    sebas arregla los cards

--- frontend/src/app/nueva-venta/nueva-venta.component.ts
+++ frontend/src/app/nueva-venta/nueva-venta.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from "@angular/core";
 import { ProductCard } from '../product-card/product-card.model'
 import { PRODUCTS } from "../mock-products";
@@ -13,9 +14,19 @@ export class NuevaVentaComponent implements OnInit {
   products = PRODUCTS;
   selectedProduct?: ProductCard;
     
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.http.get('http://localhost:3001/api/products').subscribe({
+      next: (data: any) => {
+        this.products = data;
+        console.log(data);
+      },
+      error: (err: any) => {
+        console.error(err);
+      }
+    })
+  }
 
   onSelect(product: ProductCard): void{
     this.selectedProduct = product;
