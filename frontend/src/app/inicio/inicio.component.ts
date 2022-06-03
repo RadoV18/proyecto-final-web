9cdfde7ba37e2923e7f2de6e8ee7e38940f49721
Radomir Vladislavić <vladislavic.rl@gmail.com>
2022-06-02 22:17:14 -0400

    Frontend fachero

--- frontend/src/app/inicio/inicio.component.ts
+++ frontend/src/app/inicio/inicio.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
@@ -6,8 +8,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
+  showLogin = false;
 
-  constructor() { }
+  username: string = '';
+  password: string = '';
+
+  loginAttempt: boolean = false;
+  loginValid: boolean = true;
+  isUsernameValid: boolean = true;
+  isPasswordValid: boolean = true;
+
+  toggle() {
+    this.showLogin = true;
+  }
+
+  login() {
+    this.loginAttempt = false;
+    let validInputs: boolean = true;
+    if (this.username === '') {
+      this.isUsernameValid = false;
+      validInputs = false;
+    }
+    if (this.password === '') {
+      this.isPasswordValid = false;
+      validInputs = false;
+    }
+
+    if (validInputs) {
+      const data: any = {
+        username: this.username,
+        password: this.password
+      };
+      this.loginAttempt = true;
+      this.http.post('http://localhost:3001/api/auth', data).subscribe({
+        next: (data: any) => {
+          console.log(data);
+          localStorage.setItem('username', data.username);
+          localStorage.setItem('token', data.token);
+          localStorage.setItem('name', data.name);
+          window.location.href = '/';
+        },
+        error: (err: any) => {
+          this.loginValid = false;
+        }
+      });
+    }
+
+  }
+
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
   }
